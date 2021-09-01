9d87a941acb17fd6ae06cd77561a098f62e634e6
leezon <1603963868@qq.com>
2021-09-01 17:43:56 +0800

    update

--- src/all-in.js
+++ src/all-in.js
@@ -2,3 +2,4 @@ export { default as debounce } from './modules/debounce';
 export { default as throttle } from './modules/throttle';
 export { default as getType } from './modules/getType';
 export { default as toTree } from './modules/toTree';
+export { default as verifyIdCard } from './modules/verifyIdCard';
